af67385f1d64d5937086d615586f1bd9a5944672
Shubham277353 <golubhandari405@gmail.com>
2025-08-24 22:08:31 +0530

    practice prototypes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function Books(title, authorName, numberOfPages, finished) {
-  this.title = title;
-  this.authorName = authorName;
-  this.numberOfPages = numberOfPages;
-  this.finished = finished;
-  this.bookInfo = () => {
-    return `The ${this.title} by ${this.authorName}, ${
-      this.numberOfPages
-    } pages, ${this.finished ? "read" : "not read yet"}`;
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function() {
+    console.log(this.name)
   };
 }
 
-let book = new Books("Hobbit", "J.R.R Tolkien", 295, false);
-let book2 = new Books("Kite RUnner", "Khalid Husseni", 300, true);
-let book3 = new Books("Digital Fortress", "Dan Brown", 400, true);
-console.log(book.bookInfo());
-console.log(book2.bookInfo());
-console.log(book3.bookInfo());
+Player.prototype.sayHello = function(){
+  console.log("Hello , " + this.name)
+}
+
+const player1 = new Player('steve', 'X');
+const player2 = new Player('also steve', 'O');
+
+Object.getPrototypeOf(Player.prototype);
+
+
