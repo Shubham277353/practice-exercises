b8f5d2914c630b92260d3965be0d7729f4de4660
Shubham277353 <golubhandari405@gmail.com>
2025-08-30 20:16:51 +0530

    practice js closures

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-  this.sayName = function() {
-    console.log(this.name)
+function createGreeting(greeting = "") {
+  const myGreet = greeting.toUpperCase();
+
+  return function(name) {
+    return `${myGreet} ${name}`;
   };
 }
-
-Player.prototype.sayHello = function(){
-  console.log("Hello , " + this.name)
-}
-
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
-
-Object.getPrototypeOf(Player.prototype);
-
-
