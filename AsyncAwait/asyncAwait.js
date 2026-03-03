32fb085b7c8dc062b33072facc7569bd656b3044
Shubham277353 <golubhandari405@gmail.com>
2026-03-03 12:43:22 +0530

    learn async await

---
+++ AsyncAwait/asyncAwait.js
@@ -0,0 +1,39 @@
+const server = {
+  people: [
+    {
+      name: "Odin",
+      age: 20,
+    },
+    {
+      name: "Thor",
+      age: 35,
+    },
+    {
+      name: "Freyja",
+      age: 29,
+    },
+  ],
+
+  getPeople() {
+    return new Promise((resolve, reject) => {
+      // Simulating a delayed network call to the server
+      setTimeout(() => {
+        resolve(this.people);
+      }, 2000);
+    });
+  },
+};
+
+async function getPersonInfo(name) {
+  const people = await server.getPeople();
+  const person = people.find(person => { return person.name === name });
+  return person;
+}
+
+
+async function logPerson() {
+  const person = await getPersonInfo("Thor");
+  console.log(person);
+}
+
+logPerson();
